05b0546a8093384dc57ec26a4154c50a197bae25
Simone Avancini <simone.avancini@studenti.unitn.it>
2025-02-10 09:47:32 +0100

    Google search adapter

---
+++ adapter/google_search/src/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+import uvicorn
+from googlesearch import search
+import os
+import re
+import requests
+
+
+app = FastAPI()
+
+
+@app.get('/')
+async def get_subdomains(target: str):
+    '''
+    Retrieve the list of subdomains of "target" using Google search and return them
+    as a json list
+    '''
+
+    query = f"site:{target}"
+    subdomains = set()
+
+    # Perform Google search
+    try:
+        for url in search(query, num_results=100):
+            # Extract subdomains using regex
+            subdomain_match = re.search(r'(https?://)?([a-zA-Z0-9-]+\.)*' + re.escape(target), url)
+            if subdomain_match:
+                subdomain = subdomain_match.group(2)
+                if subdomain:
+                    subdomains.add(subdomain + target)
+    except requests.exceptions.HTTPError:
+        return { 'success': False, 'message': 'Received an error from the server' }
+
+    return { 'success': True, 'subdomains': list(subdomains) }
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', default=3000))
+    uvicorn.run('main:app', host='0.0.0.0', port=port, log_level='info')
