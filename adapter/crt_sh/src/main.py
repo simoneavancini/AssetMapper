80fe4b1bec3fedfaf254325ff44b800ae6597e41
Simone Avancini <simone.avancini@studenti.unitn.it>
2025-08-14 15:37:00 +0200

    Completed crt_sh_adapter with documentation

--- adapter/crt_sh/src/main.py
+++ adapter/crt_sh/src/main.py
@@ -8,20 +8,21 @@ import os
 app = FastAPI()
 
 
-@app.get('/')
-async def get_subdomains(target: str):
+@app.get('/subdomains')
+async def get_subdomains(target: str, test: bool):
     '''
     Retrieve the list of subdomains of "target" from crt.sh and return them
     as a json list
     '''
 
-    protocol = os.environ.get('CRT_SH_PROTO', default='https')
-    domain = os.environ.get('CRT_SH_HOST', default='crt.sh')
-    port = os.environ.get('CRT_SH_PORT', default='crt.sh')
-    response = urllib.request.urlopen(f'{protocol}://{domain}:{port}/?q={target}&output=json')
+    # Sometime crt.sh can be very slow, so this can be use to test this srvice
+    if test:
+        return ['subdomain.example.com', 'blog.example.com']
+
+    response = urllib.request.urlopen(f'https://crt.sh/?q={target}&output=json')
 
     if 'application/json' not in response.headers['content-type']:
-        return { 'success': False, 'message': 'Failed to retrieve data' }
+        raise HTTPException(status_code=500, detail='Failed to retrieve data')
 
     # Parse response
     data = json.loads(response.read())
@@ -36,9 +37,9 @@ async def get_subdomains(target: str):
     # Check that the subdomains are of the right target
     true_subdomains = [sub for sub in subdomains if sub.endswith('.' + target) or sub == target]
 
-    return { 'success': True, 'subdomains': list(true_subdomains) }
+    return list(true_subdomains)
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', default=3000))
+    port = int(os.environ.get('PORT', default=80))
     uvicorn.run('main:app', host='0.0.0.0', port=port, log_level='info')
