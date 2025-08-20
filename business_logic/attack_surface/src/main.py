67874f0746c65b555203712f81f2635fcb65f85b
Simone Avancini <simone.avancini@studenti.unitn.it>
2025-08-20 10:08:50 +0200

    Completed attack surface service with documentation

--- business_logic/attack_surface/src/main.py
+++ business_logic/attack_surface/src/main.py
@@ -1,13 +1,57 @@
 from fastapi import FastAPI
 import uvicorn
-import urllib.request
+import requests
 import json
 import os
 import sys
-from services import *
 
 
 app = FastAPI()
+
+
+class GoogleSearchAdapter():
+    def __init__(self, url: str):
+        self.base_url = url
+
+    def get_subdomains(self, target: str) -> list:
+        response = requests.get(f'{self.base_url}/subdomains?target={target}')
+        if response.status_code != 200:
+            print(f'Request to "{self.base_url}" failed', file=sys.stderr)
+            return list()
+        return response.json()
+
+class CrtShAdapter():
+    def __init__(self, url: str):
+        self.base_url = url
+
+    def get_subdomains(self, target: str) -> list:
+        response = requests.get(f'{self.base_url}/subdomains?target={target}')
+        if response.status_code != 200:
+            print(f'Request to "{self.base_url}" failed', file=sys.stderr)
+            return list()
+        return response.json()
+
+class DnsAdapter():
+    def __init__(self, url: str):
+        self.base_url = url
+
+    def resolve(self, domains: list) -> list:
+        response = requests.post(f'{self.base_url}/resolve', json=domains)
+        if response.status_code != 200:
+            print(f'Request to "{self.base_url}" failed', file=sys.stderr)
+            return list()
+        return response.json()
+
+class IPinfoAdapter():
+    def __init__(self, url: str):
+        self.base_url = url
+
+    def info(self, ips: list) -> list:
+        response = requests.post(f'{self.base_url}/info', json=ips)
+        if response.status_code != 200:
+            print(f'Request to "{self.base_url}" failed', file=sys.stderr)
+            return list()
+        return response.json()
 
 
 @app.get('/')
@@ -17,17 +61,20 @@ async def scan(target: str):
     '''
     domains = [target]
 
-    # Call search adapter
-    google_domains = google_search_adapter(target)
+    # Adapters
+    google_adapter = GoogleSearchAdapter(url=os.environ.get(f'GOOGLE_SEARCH_ADAPTER'))
+    crt_sh_adapter = CrtShAdapter(url=os.environ.get(f'CRT_SH_ADAPTER'))
+    dns_adapter = DnsAdapter(url=os.environ.get(f'DNS_ADAPTER'))
+    ipinfo_adapter = IPinfoAdapter(url=os.environ.get(f'IPINFO_ADAPTER'))
 
-    # Call crt.sh adapter
-    crt_sh_domains = crt_sh_adapter(target)
+    google_domains = google_adapter.get_subdomains(target)
+    crt_sh_domains = crt_sh_adapter.get_subdomains(target)
 
     # Remove duplicates
     domains = sorted(set(domains + google_domains + crt_sh_domains))
 
     # Call DNS adapter
-    resolved_domains = dns_adapter(domains)
+    resolved_domains = dns_adapter.resolve(domains)
 
     # Extract ips
     all_ips = set()
@@ -37,12 +84,12 @@ async def scan(target: str):
     ips = list(all_ips)
 
     # Call ipinfo adapter
-    ips_info = ipinfo_adapter(ips)
+    ips_info = ipinfo_adapter.info(ips)
 
-    return { 'success': True, 'attack_surface': {
+    return {
         'domains': resolved_domains,
         'ips': ips_info
-    }}
+    }
 
 
 if __name__ == '__main__':
